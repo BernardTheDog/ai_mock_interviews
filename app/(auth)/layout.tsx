067c7ce18c2af98e1e3e6b1058563d42197fddd7
asgeir <asgeirand@gmail.com>
2025-05-14 19:01:04 +0200

    init

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import AuthForm from "@/components/AuthForm";
+import React, { ReactNode } from "react";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return <div className="auth-layout">{children}</div>;
+};
+
+export default AuthLayout;
